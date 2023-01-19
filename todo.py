8c4ffe72a5a3437edabdbced73ff034e38a9011c
yeji <ototo.coco@gmail.com>
2023-01-19 22:02:27 +0900

    todoapp

--- todo.py
+++ todo.py
@@ -1,4 +1,34 @@
 import flet as ft
 
-def main(page:)
 
+class TodoApp(ft.UserControl):
+    def build(self):
+        self.new_task = ft.TextField(hint_text="what needs to be done? ", expand=True)
+        self.tasks = ft.Column()
+        return ft.Column(
+            width=600,
+            controls=[
+                ft.Row(
+                    controls=[
+                        self.new_task,
+                        ft.FloatingActionButton(icon=ft.icons.ADD, on_click=self.add_clicked)
+                    ],
+                ),
+                self.tasks,
+            ],
+        )
+    def add_clicked(self,e):
+        self.tasks.controls.append(ft.Checkbox(label=self.new_task.value))
+        self.new_task.value = ""
+        self.update()
+def main(page:ft.Page):
+    page.title = "yeji todo app"
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.update()
+
+    todo = TodoApp()
+    todo2 = TodoApp()
+    
+
+    page.add(todo,todo2)
+ft.app(target=main)
